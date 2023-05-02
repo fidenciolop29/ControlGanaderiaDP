098f49b23ff8da63910c7d81df053bfe97dd40e5
Uriel <urielmndz55@gmail.com>
2023-05-02 11:25:30 -0600

    Prototipo 1. Interfaces iniciales

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import javax.swing.*;
+import vista.PanelInicio;
+import vista.PanelGanado;
+import vista.PanelAgregar;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        PanelInicio panel = new PanelInicio();
+        //PanelGanado panel2 = new PanelGanado();
+        //PanelAgregar panel3 = new PanelAgregar();
+
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(900,600);
+        f.add(panel);
+        //f.add(panel2);
+        //f.add(panel3);
+
+        f.setVisible(true);
+
     }
 }
